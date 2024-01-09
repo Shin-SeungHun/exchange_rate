c789047ea2065a071469fbba24fcc8fad940dff1
shin-SeungHun <sng7387@gmail.com>
2024-01-10 00:06:48 +0900

    refactor: router

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,13 +1,20 @@
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
-
+import 'package:provider/provider.dart';
 import '../ui/view/main_screen.dart';
+import '../ui/view_model/main_view_model.dart';
 
 final router = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const MainScreen(),
+      builder: (context, state) {
+        return ChangeNotifierProvider.value(
+        value: GetIt.instance<MainViewModel>(),
+        child: MainScreen(),
+      );
+      },
     ),
   ],
 );
