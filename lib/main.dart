24c03871054a5d29506889114089179469220f5f
Hyojun Cho <lionjerher84@gmail.com>
2024-01-09 18:24:24 +0900

    env 패키지 변경 건

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:exchange_rate/di/di_setup.dart';
 import 'package:flutter/material.dart';
-
 import 'router/routes.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:env_flutter/env_flutter.dart';
+
 void main() async {
   diSetup();
   await init();
   runApp(const MyApp());
 }
 
-Future<void> init() async {
-  await dotenv.load(); // dotenv 패키지를 사용하여 local.properties 파일을 로드합니다.
-  String key = dotenv.env['giphy.api.key']!; // local.properties에서 API 키를 가져옵니다.
-  String id =
-      dotenv.env['giphy.api.randomId']!; // local.properties에서 randomId를 가져옵니다.
+init() async {
+  await dotenv.load();
 
-  // ApiConfig apiConfig = GetIt.instance<ApiConfig>();
-  if (key.isNotEmpty && id.isNotEmpty) {
-    // apiConfig.apiKey = key;
-    // apiConfig.randomId = id;
-    print(key);
-    print(id);
-  }
+  print('''
+    dbPort: ${dotenv.env['DB_PORT']}
+    timeout: ${dotenv.env['TIMEOUT']}
+  ''');
 }
 
 class MyApp extends StatelessWidget {
@@ -39,4 +33,5 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
 }
