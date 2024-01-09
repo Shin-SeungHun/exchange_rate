d64c7641301a3cd3b14d4922c95b1e0c05c4226b
shin-SeungHun <sng7387@gmail.com>
2024-01-09 23:15:53 +0900

    MainViewModel 작성중

--- lib/ui/view_model/main_view_model.dart
+++ lib/ui/view_model/main_view_model.dart
@@ -1,3 +1,21 @@
+import 'package:exchange_rate/data/repository/interface/rate_repository.dart';
+import 'package:exchange_rate/ui/state/main_state.dart';
 import 'package:flutter/material.dart';
 
-class MainViewModel extends ChangeNotifier {}
+class MainViewModel extends ChangeNotifier {
+  final RateRepository rateRepository;
+
+  MainState _state = const MainState();
+
+
+  MainState get state => _state;
+
+  MainViewModel({
+    required this.rateRepository,
+  });
+
+  Future<void> updateRateResult(String baseCode) async {
+    final result = await rateRepository.getRateResult(baseCode);
+    _state =state.copyWith(rateResult: result);
+  }
+}
