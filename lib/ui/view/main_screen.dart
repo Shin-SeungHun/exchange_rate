ff4bff3a439715f4a8fd4ed3d89175e43c72b71a
shin-SeungHun <sng7387@gmail.com>
2024-01-09 15:52:14 +0900

    main_screen 화면 구성

--- lib/ui/view/main_screen.dart
+++ lib/ui/view/main_screen.dart
@@ -12,9 +12,33 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('매인 화면'),
+        leading: const FlutterLogo(),
+        title: const Text('환율 계산기'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // 기준 통화 금액 입력 필드
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: '기준 통화 금액',
+              ),
+            ),
+            // 기준 통화 드롭다운 목록
+
+            // 대상 통화 금액 입력 필드
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: '대상 통화 금액',
+              ),
+            ),
+            // 대상 통화 드롭다운 목록
+          ],
+        ),
+      ),
     );
   }
 }
