a87b8622e8a8bcd20b82635df9e338a2a175f91a
shin-SeungHun <sng7387@gmail.com>
2024-01-09 16:09:22 +0900

    dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:exchange_rate/di/di_setup.dart';
 import 'package:flutter/material.dart';
 
 import 'router/routes.dart';
-
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() async {
   diSetup();
+  await init();
   runApp(const MyApp());
+}
+
+Future<void> init() async {
+  await dotenv.load(); // dotenv 패키지를 사용하여 local.properties 파일을 로드합니다.
+  String key = dotenv.env['giphy.api.key']!; // local.properties에서 API 키를 가져옵니다.
+  String id =
+      dotenv.env['giphy.api.randomId']!; // local.properties에서 randomId를 가져옵니다.
+
+  // ApiConfig apiConfig = GetIt.instance<ApiConfig>();
+  if (key.isNotEmpty && id.isNotEmpty) {
+    // apiConfig.apiKey = key;
+    // apiConfig.randomId = id;
+    print(key);
+    print(id);
+  }
 }
 
 class MyApp extends StatelessWidget {
