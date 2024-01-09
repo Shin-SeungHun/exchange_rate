0ef9b5f5b4bbf16d0aa42ce62d860c7ef41aafda
shin-SeungHun <sng7387@gmail.com>
2024-01-09 23:14:52 +0900

    fix api

--- lib/data/data_source/exchange_rate_api.dart
+++ lib/data/data_source/exchange_rate_api.dart
@@ -7,7 +7,6 @@ class ExchangeRateApi {
   getExchangeResult(String baseCode) async {
     final response = await http
         .get(Uri.parse('https://open.er-api.com/v6/latest/$baseCode'));
-    // // https://v6.exchangerate-api.com/v6/f1da1d0984279f9e565c3c2f/latest/USD
     return ExchangeRateDto.fromJson(jsonDecode(response.body));
   }
 }
